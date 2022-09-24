686b36c3ce040c9524d558632cbb7b32b62b177c
AREmondev <abdurrahmanemondev@gmail.com>
2022-09-24 07:46:06 +0600

    some change

--- pages/order/index.vue
+++ pages/order/index.vue
@@ -19,6 +19,49 @@
                 </div>
             </form>
             <Order :userId="userId" :products="products" />
+
+            <div class="row">
+                <div class="col-xl-12">
+                    <div class="card">
+                        <div class="card-body">
+                            <h4 class="mt-0 header-title mb-4">Latest Trasaction</h4>
+                            <div class="table-responsive">
+                                <div class="p-3">
+                                    <button @click.prevent="transitionType = 'order'" :class="{'btn-primary' : transitionType == 'order'}" class="btn">Order</button>
+                                    <button @click.prevent="transitionType = 'payment'" :class="{'btn-primary' : transitionType == 'payment'}"   class="btn">Payment</button>
+                                    <!-- <button  @click.prevent="transitionType = 'stock'" :class="{'btn-primary' : transitionType == 'stock'}"  class="btn">Stock</button> -->
+                                </div>
+                                <table class="table table-hover">
+                                    <thead>
+                                        <tr v-if="transitionType != 'stock'">
+                                            <th scope="col">ID No.</th>
+                                            <th scope="col">Date</th>
+                                            <th scope="col">Order Due</th>
+                                        </tr>
+                                        <tr v-else>
+                                            <th scope="col">ID No.</th>
+                                            <!-- <th scope="col">Name</th> -->
+                                            <th scope="col">Date</th>
+                                            <!-- <th scope="col">Order Due</th> -->
+                                            <th scope="col">Payment Due</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody v-if="orders && orders.length">
+                                        <tr v-if="order.type == transitionType" :class="{'active' : order.type === 'Payment'}" v-for="(order, i) in orders" :key="i">
+                                            <th scope="row">#{{i + 1}}</th>
+                                            <td v-if="order.totaDue">{{order.totaDue}}</td>
+                                            <td>{{order.createdAt.split("T")[0].split("-").reverse().join("/")}}</td>
+                                            <td>{{order.payment || order.due}}Tk</td>
+                                            <!-- <td v-if="order.customer">{{order.customer.total_due}}Tk</td> -->
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
         <div v-else class="container py-5">
             <input  class="form-control mb-5"  v-model="customer" type="text" name="" id="">
@@ -48,6 +91,8 @@
                 
                 </tbody>
             </table>
+
+           
         </div>
     </client-only>
 </template>
@@ -62,7 +107,9 @@ export default {
             ordererUser: null,
             products: [],
             userId: this.$route.query.id,
-            customers: []
+            customers: [],
+            orders: null,
+            transitionType: 'order',
         }
     },
     created() {
@@ -99,7 +146,8 @@ export default {
                     this.$router.push('/order?id='+customerId)
                     console.log(response)
                     if(response.status == 200){
-                    this.ordererUser = response.data.customer;
+                        this.ordererUser = response.data.customer;
+                        this.orders = response.data.customer.transitions
                     }
                 }).catch(err => console.log(err));
             }
