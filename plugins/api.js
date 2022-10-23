b2a1d6debd1432a4717caf0e4da63560f85211c4
AREmondev <abdurrahmanemondev@gmail.com>
2022-10-23 16:38:22 +0600

    backend link

--- plugins/api.js
+++ plugins/api.js
@@ -1,5 +1,5 @@
 export const API_URL =(url)=>{
-  // 'https://dokan-api.herokuapp.com/api/' + url;
-  let absoluteUrl = 'http://localhost:8000/api/' + url;
+  let absoluteUrl = 'https://dokan-api.herokuapp.com/api/' + url;
+  // let absoluteUrl = 'http://localhost:8000/api/' + url;
   return absoluteUrl;
 }
